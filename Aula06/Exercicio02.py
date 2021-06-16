ef57651f7a9fa505f61838f8148b594cc4f5acac
Lucas <lucasmbrute614@gmail.com>
2021-06-16 02:37:43 -0300

    Resolução de exercícios

--- Aula06/Exercicio02.py
+++ Aula06/Exercicio02.py
@@ -1,2 +1,11 @@
 # 02 - Desenvolva um código que pergunte um número n para o 
 # usuário e exiba todos seus divisores
+
+#n = (input("Digite um número aqui: "))
+
+lista = [21,23,24,"alo"]
+
+for i in lista:
+    print(lista)
+    print()
+    print(i)
--- Aula06/Exercicio03.py
+++ Aula06/Exercicio03.py
@@ -1,2 +1,4 @@
-# 03 - Faça um script que o usuário escolha um número de início, um número de fim, e um número de passo. O programa deve printar todos os números 
-# do intervalo entre início e fim, "pulando" de acordo com o intervalo passado.
+# 03 - Faça um script que o usuário escolha um número de início, 
+# um número de fim, e um número de passo. O programa deve printar 
+# todos os números do intervalo entre início e fim, "pulando" de 
+# acordo com o intervalo passado.
