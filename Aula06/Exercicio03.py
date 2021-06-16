b18403ed332e1f91c7f4ba84fdfb17bbc3d3a4fc
Lucas <lucasmbrute614@gmail.com>
2021-06-16 02:43:34 -0300

    Exercício

--- Aula06/Exercicio03.py
+++ Aula06/Exercicio03.py
@@ -2,3 +2,10 @@
 # um número de fim, e um número de passo. O programa deve printar 
 # todos os números do intervalo entre início e fim, "pulando" de 
 # acordo com o intervalo passado.
+
+numero_inicio = int(input("Digite o número de início: "))
+numero_fim = int(input("Digite o número do fim: "))
+numero_passo = int(input("Digte o número de passo: "))
+
+for i in range(numero_inicio, numero_fim,numero_passo):
+    print(i)
