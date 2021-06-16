1c7f64e66815b5f365543a1925db924e278b6c93
Lucas <lucasmbrute614@gmail.com>
2021-06-16 17:47:51 -0300

    Exercício FOR

--- Aula06/Exercicio04.py
+++ Aula06/Exercicio04.py
@@ -1,8 +1,17 @@
 # 04 - Desenvolva um código em que o usuário vai entrar vários números e no final vai apresentar a 
 # soma deles (o usuário vai dizer quantos números serão informados antes de começar)
 
-n = int(input("Digite os números: "))
+cont = 0
+n = float(input("Digite o número aqui : "))
+cont += n
+continuar = (input("Deseja continuar digitando os números ? [SIM / NÃO] "))
+while continuar.upper().replace("s", "sim") == "SIM":
+    n = float(input("Digite o número aqui : "))
+    cont += n
+    continuar = (input("Deseja continuar digitando os números ? [SIM / NÃO] "))
+    if continuar.upper().replace("nao", "não" ) == "NÃO":
+        print("Obrigado.")
+        break         
+print(f"A soma dos valores foi resultou em {cont}")
 
-for i in n:
-    print(i)
 
