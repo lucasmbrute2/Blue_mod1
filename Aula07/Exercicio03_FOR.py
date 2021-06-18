e622c602b327cb100424cfc4b283a2ac8d4cfe51
Lucas <lucasmbrute614@gmail.com>
2021-06-18 12:30:29 -0300

    Colab

--- Aula07/Exercicio03_FOR.py
+++ Aula07/Exercicio03_FOR.py
@@ -1,3 +1,12 @@
 # #03 - Crie um programa que leia o ano de nascimento de sete pessoas. No final,
 # mostre quantas pessoas ainda não atingiram a maioridade e quantas já são
 # maiores.
+
+
+pessoas = 0
+while pessoas < 3:
+    ano = int(input("Digite o a ano de sua idade: "))
+    pessoas += 1
+    
+    
+for i in ano:
