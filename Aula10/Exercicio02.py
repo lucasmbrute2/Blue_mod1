6dafa36a5f67fc2cea4d38222d86208195d3d6a0
Lucas <lucasmbrute614@gmail.com>
2021-06-20 02:07:20 -0300

    Exercícios

---
+++ Aula10/Exercicio02.py
@@ -0,0 +1,6 @@
+# 2 - Faça um programa que leia nome e altura de várias pessoas, guardando tudo em uma lista, 
+# depois do dado inserido, pergunte ao usuário se ele quer continuar, se ele não quiser pare o 
+# programa. No final mostre:
+# # Quantas pessoas foram cadastradas
+# # Mostre a maior altura
+# # Mostre a menor altura
