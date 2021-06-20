4996b91e88b68b57d40223d336d78a6f89016f2b
Lucas <lucasmbrute614@gmail.com>
2021-06-20 03:11:15 -0300

    Desafio caixa eletrônico

--- Projetos/caixa_eletronico.py
+++ Projetos/caixa_eletronico.py
@@ -9,38 +9,51 @@
 
 # Exemplo 2: Para sacar a quantia de 399 reais, o programa fornece três notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
 
-n1 = 1
-n5= 5
-n10 = 10
-n50 = 50
-n100 = 100
-valor_restante = 0
-valor_final = 0
+n1 = 0
+n5= 0
+n10 = 0
+n50 = 0
+n100 = 0
 
+valor_n1 = 1
+valor_n2 = 5
+valor_n3 = 10
+valor_n4 = 50
+valor_n5 = 100
 
 valor_saque = int(input("Digite o valor do saque: "))
 while valor_saque <10 or valor_saque >600:
     valor_saque = int(input("Valor indisponível. Digite um valor entre [10] e [600] "))
 
-
-valor_restante = valor_saque//n100
-valor_final = valor_saque - valor_restante*100
-
-valor_restante = valor_saque//n50
-valor_final = valor_saque - valor_restante*50
-print(valor_final)
-
-valor_restante = valor_saque//10
-valor_final = valor_saque - valor_restante*10
-print(valor_final)
-
-valor_restante = valor_saque//5
-valor_final = valor_saque - valor_restante*5
-print(valor_final)
-
-valor_restante = valor_saque//1
-valor_final = valor_saque - valor_restante*1
-print(valor_final)
+saque = valor_saque
 
 
 
+
+n100 = saque//valor_n5
+saque = saque - (n100*valor_n5)
+
+n50 = saque//valor_n4
+saque = saque - (n50*valor_n4)
+
+n10 = saque//valor_n3
+saque = saque - (n10*valor_n3)
+n5 = saque//valor_n2
+saque = saque - (n5*valor_n2)
+
+n1 = saque//valor_n1
+saque = saque - (n1*valor_n1)
+
+print(f"O valor total do saque foi de R${valor_saque}\n")
+
+if n100 >0:
+
+    print(f"Foram sacadas {n100} nota(s) de {valor_n5} reais.")
+if n50 >0:
+    print(f"Foram sacadas {n50} nota(s) de {valor_n4} reais.")
+if n10>0:
+    print(f"Foram sacadas {n10} nota(s) de {valor_n3} reais.")
+if n5>0:
+    print(f"Foram sacadas {n5} nota(s) de {valor_n2} reais. ")
+if n1 >0:
+    print(f"Foram sacadas {n1} nota(s) de {valor_n1} real.")
