69cb6d3f918cc7a42be129cd288a3ab770c9ee54
Lucas <lucasmbrute614@gmail.com>
2021-06-21 19:48:48 -0300

    Exercícios

--- Aula10/Exercicio02.py
+++ Aula10/Exercicio02.py
@@ -4,3 +4,29 @@
 # # Quantas pessoas foram cadastradas
 # # Mostre a maior altura
 # # Mostre a menor altura
+total = 0
+l = []
+maior = 0
+menor = 1000000000000
+cont = 0
+while True:
+    total += 1
+    nome = (input("Digite seu nome: "))
+    l.append(nome)
+    altura = (float(input("Digite sua altura: ")))
+    l.append(altura)
+    if altura < menor:
+        menor = altura
+    if altura > maior:
+        maior = altura 
+    continuar = input("Você quer continuar ? ")
+    if continuar.upper() == "SIM" or continuar.upper() == "S":
+        continue
+    else:
+        break
+print()   
+print("Obrigado por participar!!")
+print()
+print(f"O total de pessoa(s) cadastrada(s) foi de: {total} pessoa(s).")
+print(f"A menor altura foi: {menor} metros.")
+print(f"A maior altura foi: {maior} metros.")
