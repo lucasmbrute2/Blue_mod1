1d1a9f5f7e7cc3aa4ae615018d4bf6412215f4fd
Lucas <lucasmbrute614@gmail.com>
2021-06-20 23:13:20 -0300

    Desafio MEGA SENA

--- Aula09/Desafio.py
+++ Aula09/Desafio.py
@@ -3,6 +3,20 @@
 # números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta
 
 
-import random
-Q_jogos = int(input("Quantos jogos você quer gerar? "))
+from random import randint
 
+
+jogos = int(input("Digite quantos jogos você deseja. "))
+l= []
+l_valores = []
+for i in range(jogos):
+    l = []
+    while len(l) <=6:
+        num = randint(1,60)
+        if num not in l:
+            l.append(num)
+        
+    l_valores.append(l)
+
+
+    print(f" O jogo {i+1} saiu com os seguintes valores: {l}")
