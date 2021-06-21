078f7e539ef8988796f031ba80c6952fa26163e5
Lucas <lucasmbrute614@gmail.com>
2021-06-21 00:05:07 -0300

    Desafio Forca

--- Aula07/Desafio.py
+++ Aula07/Desafio.py
@@ -2,16 +2,24 @@
 # 0 e 10. O jogador vai tentar adivinhar qual número foi escolhido até acertar, entre os
 # palpites diga ao jogador se o número do computador é maior ou menor ao que ele
 # digitou,no final mostre quantos palpites foram necessários para vencer.
-# #05 (DESAFIO) Em uma eleição presidencial existem quatro candidatos. Os votos
-# são informados por meio de código.
-# Os códigos utilizados são:
-# 1 , 2, 3 - Votos para os respectivos candidatos
-# (você deve montar a tabela ex: 1 - José / 2- João / etc)
-# 5 - Voto Nulo
-# 6 - Voto em Branco
-# Faça um programa que calcule e mostre:
-# O total de votos para cada candidato;
-# O total de votos nulos;
-# O total de votos em branco;
-# A percentagem de votos nulos sobre o total de votos;
-# A percentagem de votos em branco sobre o total de votos
+
+
+from random import randint
+again = "Digite novamente:"
+num = randint(0,10)
+cont = 0
+guess = int(input("Tente adivinhar o número entre 0 e 10 em que estou pensando: "))
+cont = 0
+while  guess <0 or guess >10:
+        guess = int(input("Só vale números de 0 a 10, diga novamente."))
+
+while True:
+    cont +=1
+    if guess>num:
+        guess = int(input(f"Hmm, tente pensar em um valor menor. {again} "))
+    elif guess<num:
+        guess = int(input(f"Hmm, tente pensar um pouco mais alto. {again} "))
+    else:
+        print(f"Parabéns, você acertou!!Eu tinha pensado no número {num}")
+        break
+print(f"Você deu {cont} palpite(s) até acertar.")
