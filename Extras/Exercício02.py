71c05370070da22d8dfbbab9a77b9aa933b1acfc
Lucas <lucasmbrute614@gmail.com>
2021-06-21 22:22:53 -0300

    Exercícios

--- Extras/Exercício02.py
+++ Extras/Exercício02.py
@@ -1,29 +1,30 @@
 # Crie um programa onde o usuário possa digitar vários valores numérios e cadestre-os em uma lista. Caso o número já exista lá dentro
 # , ele NÃO será adicionado. No final serão exibidos todos os valores únicos digitados, em ordem crescente. 
 
-l = []
-continuar = "sim"
-while continuar.lower() == "sim" or continuar.lower() == "s":
-    num = (int(input("Digite aqui o número: ")))
-    if num not in l:
-        l.append(num)
-    continuar = input("Você deseja continuar ? ")
-    
-l.sort()
-print(f"Todos os valores digitados em ordem crescente ficaram assim: {l}")
-
-# # OUTRA FORMA MELHOR, PORÉM A LINHA 26 NÃO FUNCIONA:
-
 # l = []
-# while True:
+# continuar = "sim"
+# while continuar.lower() == "sim" or continuar.lower() == "s":
 #     num = (int(input("Digite aqui o número: ")))
 #     if num not in l:
 #         l.append(num)
-#         print("Valor adicionado com sucesso.")
-#     else:
-#         print("Valor duplicado, não vou adicionar.")
-#     continuar = (input("Você deseja continuar ? "))
-#     if continuar in 'Nn':
-#         break
+#     continuar = input("Você deseja continuar ? ")
+    
 # l.sort()
 # print(f"Todos os valores digitados em ordem crescente ficaram assim: {l}")
+
+# # OUTRA FORMA MELHOR, PORÉM A LINHA 26 NÃO FUNCIONA:
+
+l = []
+while True:
+    num = (int(input("Digite aqui o número: ")))
+    if num not in l:
+        l.append(num)
+        print("Valor adicionado com sucesso.")
+    else:
+        print("Valor duplicado, não vou adicionar.")
+    continuar = (input("Você deseja continuar ? "))
+    if continuar in ['N','n','Não','NÃO']:
+        break
+
+l.sort()
+print(f"Todos os valores digitados em ordem crescente ficaram assim: {l}")
