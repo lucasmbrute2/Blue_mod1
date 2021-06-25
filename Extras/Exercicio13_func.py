d877e94dc042d60f9bb75eec6277173e7b67094b
Lucas <lucasmbrute614@gmail.com>
2021-06-25 18:50:21 -0300

    Exercícios

---
+++ Extras/Exercicio13_func.py
@@ -0,0 +1,20 @@
+# Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores
+# inteiros.
+# Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+def maior(num):
+    maior = 0
+    if num > maior:
+        maior = num
+    return f"O maior valor é {maior}"
+   
+
+while True:
+    valor = int(input("Digite o valor: "))
+    maior(valor)
+    continuar = input("Você quer continuar informando valores: ").upper()[0]
+    if continuar in ['S']:
+        continue
+    else: 
+        break
+
+print(maior(valor))
