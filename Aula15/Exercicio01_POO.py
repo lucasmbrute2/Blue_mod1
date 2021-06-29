3570ad313fc71e83c1c03c3005ca8e7d1670d5a4
Lucas <lucasmbrute614@gmail.com>
2021-06-29 20:32:18 -0300

    Exercicios

--- Aula15/Exercicio01_POO.py
+++ Aula15/Exercicio01_POO.py
@@ -1,3 +1,19 @@
 # A) Crie uma classe pessoa com os seguintes atributos: nome, sobrenome e idade. 
 # B)Acrescente a classe criada um método para mostrar os dados de uma pessoa. 
 # C)Crie um objeto do tipo pessoa para testar suas propriedades e métodos
+
+class Pessoa():
+    def __init__(self,nome,sobrenome,idade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+    def mostrar_dados(self):
+        print(f"Nome completo: {self.nome} {self.sobrenome}")
+        print(f"Idade: {self.idade}")
+
+
+pessoa = Pessoa("Lucas", "Souza", 21)
+
+pessoa.mostrar_dados()
+    
