87a053c0dce01c55b4e11f3882938cb888b60a05
Lucas <lucasmbrute614@gmail.com>
2021-06-29 20:42:15 -0300

    Exercícios

--- Aula15/Exercicio02.POO.py
+++ Aula15/Exercicio02.POO.py
@@ -2,3 +2,23 @@
 # uma conta corrente. Sua classe deverá ter os atributos Titular e
 # Saldo, e os métodos Sacar e Depositar. Crie um objeto da classe
 # Conta e teste os atributos e métodos implementados.
+
+class Conta():
+
+    def __init__(self,titular,saldo):
+
+        self.titular = titular
+        self.saldo = saldo
+
+    
+    def sacar(self,valor):
+        self.saldo -= valor
+    
+    def depositar(self,valor):
+        self.saldo += valor
+
+
+conta = Conta("Lucas", 3500)
+
+conta.sacar(1000)
+print(f"O titular é {conta.titular} e seu saldo é: {conta.saldo}")
