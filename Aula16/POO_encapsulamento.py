020babe8ad79990e54561d38afd62063972a0448
Lucas <lucasmbrute614@gmail.com>
2021-07-02 13:52:50 -0300

    Exercícios

--- Aula16/POO_encapsulamento.py
+++ Aula16/POO_encapsulamento.py
@@ -7,7 +7,9 @@
 # foi colocada no veículo.
 # ii. abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível e mostra o valor
 # a ser pago pelo cliente.
-# iii. alterarValor( ) – altera o valor do litro do combustível.
+# 
+# 
+# iii. alterarValor( ) – altera o valor do litro do combustível. //
 # iv. alterarCombustivel( ) – altera o tipo do combustível.
 # v. alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
 # OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba.
@@ -18,18 +20,28 @@ class Bomba_combustivel:
         self.valor_litro = valor_litro
         self.quantidade_combustivel = quantidade_combustivel
         
+        self.litros = self.quantidade_combustivel/self.valor_litro
+        self.quantidade_bomba = 50
     
-    def abastecer_por_valor(self,):
-        self.quantidade_combustivel/self.valor_litro
+    
+    def abastecer_por_valor(self):
+            print(f"Foi abastecido R$ {self.quantidade_combustivel}. O tanque foi preenchido com {self.litros:.1f} litros.")
+            print(f"A bomba possui ainda {self.quantidade_bomba - self.litros:.2f} litros") 
 
     def abastecer_por_litro(self):
+        
+        print(f"O tanque será abastecido com {self.litros:.1f} litros. O valor será R$ {self.quantidade_combustivel}  ")
+        
+ 
+    
 
-    def alterar_valor(self):
 
-    def alterar_tipo_combustivel(self):
+bomba = Bomba_combustivel("Etanol", 4.50, 50 )
 
-    def alterar_quantidade_combustivel(self):
+bomba.abastecer_por_valor()
+
+bomba.abastecer_por_litro()
 
 
-abastecer = Bomba_combustivel("Etanol", 4.50, 50 )
+
 
