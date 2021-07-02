caebe7d6d579621ce84816b80f6801344bbadd1c
Lucas <lucasmbrute614@gmail.com>
2021-07-01 22:53:36 -0300

    Exercícios

--- Aula11/Exercício02.py
+++ Aula11/Exercício02.py
@@ -4,10 +4,21 @@
 # - Quantidade solicitada não disponível.
 
 estoque = {"vassoura": 3 ,"panela": 10, "arroz": 8, "salsisha": 15, "pão": 30, "chocolate": 6}
+baixa_estoque = []
+total_quantidade_geral = 0
 
-print("Bem-Vindo ao nosso supermercado, os nossos produtos são:\n-> [VASSOURA]\n-> [PANELA] \n-> [ARROZ] \n-> [SALSISHA] \n-> [PÃO] \n-> [CHOCOLATE]")
+continuar = input("Bem-vindo (a) ao Supermercado. Deseja ir as compras? (s/n) ").lower()
+while continuar not in ['s', 'n']:
+    continuar = input("Resposta inválida. Deseja ir as compras ? (s/n ").lower()
 
-comprar = input("Digite o que você quer comprar: ").lower()
+while continuar == 's':
+    print("Bem-Vindo ao nosso supermercado, os nossos produtos são:\n-> [VASSOURA]\n-> [PANELA] \n-> [ARROZ] \n-> [SALSISHA] \n-> [PÃO] \n-> [CHOCOLATE]")
+
+
+
+
+
+
 if comprar not in estoque:
     print("Produto inválido")
 
@@ -15,8 +26,8 @@ quantidade = int(input("Quantidade da compra: "))
 
 for i in estoque:
     if i == comprar:
-        
-        
+        baixa_estoque[i] = quantidade
+        estoque.update(baixa_estoque)
 
 
 print(estoque)
