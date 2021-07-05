f03dee108e5cfc370e85a630cf24cc8900f36b1c
Lucas <lucasmbrute614@gmail.com>
2021-07-04 23:05:34 -0300

    Projeto Final

--- Projetos/Projeto_final.py
+++ Projetos/Projeto_final.py
@@ -25,7 +25,65 @@
 #Quanto mais tempo passa melhor a bebida fica, tipo um vinho rs.
 #Uma sociedade de bebidas, em que elas são premiadas por deixar o usuario embreagado.
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# A Vida de Joe 
+ 
+# Contexto : Joe é um viralata magricelo, que se perdeu de sua casa e vive numa viela do centro de belo horizonte. Porém a gestão publica da cidade não permite animais de rua, e todos os dias a carrocinha passa procurando cachorros para trancar nas gaiolas de um péssimo abrigo. Sua missão é levar joe de volta a sua casa, sem que ele seja pego pela malvada carrocinha de BH. 
+from time import sleep
+class Opcoes:
+    def __init__(self,opcoes):
+        self.opcoes = opcoes
+        
+    def retorna_dic(self,)
+        
 
-if(__name__ == "__main__"):
+class Temporizador:
+    def __init__(self):
+        pass
+    
+    def atrasa_dialogo(self,atrasa_dialogo):
+        for i in atrasa_dialogo:
+            print(i,end="")
+            sleep(0.01)
+       
 
+        
+class Horas():
+    def __init__(self):
+        self.horas = 6
+        self.minutos = 0
+    
+    def __str__(self):
+        return f"São {self.horas:02d}:{self.minutos:02d}"
+
+
+class Personagem():
+    def __init__(self):
+        self.sono = True
+        self.fome = True
+        self.sozinho = True
+        self.sede = True
+    
+    def __str__(self):
+        return f"--> {nome} está {'com sono' if self.sono  else 'sem sono'}, {'com fome' if self.fome else 'sem fome'}, {'com sede' if self.sede else 'hidratado'} e {'se sentindo sozinho.' if self.sozinho else 'se sentindo acolhido.'}"
+
+dialogo = Temporizador()
+nome = "Joe"
+personagem = Personagem()
+horas = Horas()
+opcoes = Opcoes()
+
+
+if __name__=="__main__":
+
+    dic = {1: "Encontrar outro lugar para continuar dormindo", 2: "Aguardar com sono a ong abrir.",3: " Voltar a procurar sua casa. ",  4: "Se esconder da carrocinha"  }
     dia = 1
+    descricao = f"{horas} do dia {dia}. Você é um viralata magricelo, que se perdeu de casa e vive numa viela muito barulhenta, e para piorar (sempre pode piorar rs) a carrocinha passa as 6:30 recolhendo cachorros de rua. Seu nome é {nome} e você procura a sua casa todos os dias. Entretando a ONG em que você geralmente se alimenta abre as 7:00. O que deseja fazer agora?"
+    
+    status = f"\n{personagem}"
+    print('='*70)
+    dialogo.atrasa_dialogo(descricao)
+    print()
+    dialogo.atrasa_dialogo(status)
+    opcoes.retorna_dic()
+
